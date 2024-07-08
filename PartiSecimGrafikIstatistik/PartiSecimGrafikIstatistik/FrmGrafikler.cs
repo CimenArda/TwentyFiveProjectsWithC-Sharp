6d414d43cb024eec37f9f1be692535818458bbe3
ArdaCimen <cimenarda.yaz@gmail.com>
2024-07-08 13:34:38 +0300

    PartiSecimGrafikIstatistik Eklemeler

--- PartiSecimGrafikIstatistik/PartiSecimGrafikIstatistik/FrmGrafikler.cs
+++ PartiSecimGrafikIstatistik/PartiSecimGrafikIstatistik/FrmGrafikler.cs
@@ -41,9 +41,45 @@ namespace PartiSecimGrafikIstatistik
 
 
             //Grafiğe Toplam Sonuçları Getirme
+            baglanti.Open();
+            SqlCommand komut2 = new SqlCommand("Select SUM(AParti),Sum(BParti),SUM(CParti),SUM(DParti),SUM(EParti) from TBLILCE", baglanti);
 
+            SqlDataReader dr2  =komut2.ExecuteReader();
+            while(dr2.Read()) 
+            {
 
+                chart1.Series["Partiler"].Points.AddXY("A PARTİ", dr2[0]);
+                chart1.Series["Partiler"].Points.AddXY("B PARTİ", dr2[1]);
+                chart1.Series["Partiler"].Points.AddXY("C PARTİ", dr2[2]);
+                chart1.Series["Partiler"].Points.AddXY("D PARTİ", dr2[3]);
+                chart1.Series["Partiler"].Points.AddXY("E PARTİ", dr2[4]);
+            }
+            baglanti.Close();
 
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            baglanti.Open();
+
+            SqlCommand komut = new SqlCommand("Select * from TBLILCE where IlceAd=@p1", baglanti);
+            komut.Parameters.AddWithValue("@p1", comboBox1.Text);
+            SqlDataReader dr = komut.ExecuteReader();
+            while (dr.Read())
+            {
+                progressBar1.Value = int.Parse(dr[2].ToString());
+                progressBar2.Value = int.Parse(dr[3].ToString());
+                progressBar3.Value = int.Parse(dr[4].ToString());
+                progressBar4.Value = int.Parse(dr[5].ToString());
+                progressBar5.Value = int.Parse(dr[6].ToString());
+
+                lbla.Text = dr[2].ToString();
+                lblb.Text = dr[3].ToString();
+                lblc.Text = dr[4].ToString();
+                lbld.Text = dr[5].ToString();
+                lble.Text = dr[6].ToString();
+            }
+            baglanti.Close();
         }
     }
 }
