e798ad64c94a3fd8470d7af9d815e09dc8da16ba
ArdaCimen <cimenarda.yaz@gmail.com>
2024-07-15 11:44:51 +0300

    Veri Tabanı Verilerini Şifreleme  Uygulaması

---
+++ VerileriSifrele/VerileriSifrele/Form1.Designer.cs
@@ -0,0 +1,295 @@
+﻿namespace VerileriSifrele
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtAd = new System.Windows.Forms.TextBox();
+            this.txtSoyad = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtMail = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtSifre = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.TxtHesapNo = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.veriTabaniSifrelemeProje13DataSet = new VerileriSifrele.VeriTabaniSifrelemeProje13DataSet();
+            this.tBLVERILERBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.tBLVERILERTableAdapter = new VerileriSifrele.VeriTabaniSifrelemeProje13DataSetTableAdapters.TBLVERILERTableAdapter();
+            this.ıDDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.aDDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.sOYADDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.mAILDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.sIFREDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.hESAPNODataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.veriTabaniSifrelemeProje13DataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tBLVERILERBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ıDDataGridViewTextBoxColumn,
+            this.aDDataGridViewTextBoxColumn,
+            this.sOYADDataGridViewTextBoxColumn,
+            this.mAILDataGridViewTextBoxColumn,
+            this.sIFREDataGridViewTextBoxColumn,
+            this.hESAPNODataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.tBLVERILERBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 239);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(800, 199);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Ad:";
+            // 
+            // txtAd
+            // 
+            this.txtAd.Location = new System.Drawing.Point(83, 30);
+            this.txtAd.Name = "txtAd";
+            this.txtAd.Size = new System.Drawing.Size(166, 22);
+            this.txtAd.TabIndex = 2;
+            // 
+            // txtSoyad
+            // 
+            this.txtSoyad.Location = new System.Drawing.Point(83, 67);
+            this.txtSoyad.Name = "txtSoyad";
+            this.txtSoyad.Size = new System.Drawing.Size(166, 22);
+            this.txtSoyad.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Soyad:";
+            // 
+            // txtMail
+            // 
+            this.txtMail.Location = new System.Drawing.Point(83, 106);
+            this.txtMail.Name = "txtMail";
+            this.txtMail.Size = new System.Drawing.Size(166, 22);
+            this.txtMail.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(27, 112);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 16);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Mail:";
+            // 
+            // txtSifre
+            // 
+            this.txtSifre.Location = new System.Drawing.Point(352, 30);
+            this.txtSifre.Name = "txtSifre";
+            this.txtSifre.Size = new System.Drawing.Size(166, 22);
+            this.txtSifre.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(286, 30);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(37, 16);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Şifre:";
+            // 
+            // TxtHesapNo
+            // 
+            this.TxtHesapNo.Location = new System.Drawing.Point(352, 73);
+            this.TxtHesapNo.Name = "TxtHesapNo";
+            this.TxtHesapNo.Size = new System.Drawing.Size(166, 22);
+            this.TxtHesapNo.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(286, 79);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(69, 16);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "HesapNo:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(353, 119);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 39);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Kaydet";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(604, 30);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(44, 16);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "label6";
+            // 
+            // veriTabaniSifrelemeProje13DataSet
+            // 
+            this.veriTabaniSifrelemeProje13DataSet.DataSetName = "VeriTabaniSifrelemeProje13DataSet";
+            this.veriTabaniSifrelemeProje13DataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // tBLVERILERBindingSource
+            // 
+            this.tBLVERILERBindingSource.DataMember = "TBLVERILER";
+            this.tBLVERILERBindingSource.DataSource = this.veriTabaniSifrelemeProje13DataSet;
+            // 
+            // tBLVERILERTableAdapter
+            // 
+            this.tBLVERILERTableAdapter.ClearBeforeFill = true;
+            // 
+            // ıDDataGridViewTextBoxColumn
+            // 
+            this.ıDDataGridViewTextBoxColumn.DataPropertyName = "ID";
+            this.ıDDataGridViewTextBoxColumn.HeaderText = "ID";
+            this.ıDDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.ıDDataGridViewTextBoxColumn.Name = "ıDDataGridViewTextBoxColumn";
+            this.ıDDataGridViewTextBoxColumn.ReadOnly = true;
+            this.ıDDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // aDDataGridViewTextBoxColumn
+            // 
+            this.aDDataGridViewTextBoxColumn.DataPropertyName = "AD";
+            this.aDDataGridViewTextBoxColumn.HeaderText = "AD";
+            this.aDDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.aDDataGridViewTextBoxColumn.Name = "aDDataGridViewTextBoxColumn";
+            this.aDDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // sOYADDataGridViewTextBoxColumn
+            // 
+            this.sOYADDataGridViewTextBoxColumn.DataPropertyName = "SOYAD";
+            this.sOYADDataGridViewTextBoxColumn.HeaderText = "SOYAD";
+            this.sOYADDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.sOYADDataGridViewTextBoxColumn.Name = "sOYADDataGridViewTextBoxColumn";
+            this.sOYADDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // mAILDataGridViewTextBoxColumn
+            // 
+            this.mAILDataGridViewTextBoxColumn.DataPropertyName = "MAIL";
+            this.mAILDataGridViewTextBoxColumn.HeaderText = "MAIL";
+            this.mAILDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.mAILDataGridViewTextBoxColumn.Name = "mAILDataGridViewTextBoxColumn";
+            this.mAILDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // sIFREDataGridViewTextBoxColumn
+            // 
+            this.sIFREDataGridViewTextBoxColumn.DataPropertyName = "SIFRE";
+            this.sIFREDataGridViewTextBoxColumn.HeaderText = "SIFRE";
+            this.sIFREDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.sIFREDataGridViewTextBoxColumn.Name = "sIFREDataGridViewTextBoxColumn";
+            this.sIFREDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // hESAPNODataGridViewTextBoxColumn
+            // 
+            this.hESAPNODataGridViewTextBoxColumn.DataPropertyName = "HESAPNO";
+            this.hESAPNODataGridViewTextBoxColumn.HeaderText = "HESAPNO";
+            this.hESAPNODataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.hESAPNODataGridViewTextBoxColumn.Name = "hESAPNODataGridViewTextBoxColumn";
+            this.hESAPNODataGridViewTextBoxColumn.Width = 125;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.TxtHesapNo);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtSifre);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtMail);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtSoyad);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtAd);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.veriTabaniSifrelemeProje13DataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tBLVERILERBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtAd;
+        private System.Windows.Forms.TextBox txtSoyad;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtMail;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtSifre;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox TxtHesapNo;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label6;
+        private VeriTabaniSifrelemeProje13DataSet veriTabaniSifrelemeProje13DataSet;
+        private System.Windows.Forms.BindingSource tBLVERILERBindingSource;
+        private VeriTabaniSifrelemeProje13DataSetTableAdapters.TBLVERILERTableAdapter tBLVERILERTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ıDDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn aDDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn sOYADDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn mAILDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn sIFREDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn hESAPNODataGridViewTextBoxColumn;
+    }
+}
+
---
+++ VerileriSifrele/VerileriSifrele/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+namespace VerileriSifrele
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlConnection baglanti = new SqlConnection("Data Source=ARDACIMEN\\SQLEXPRESS;Initial Catalog=VeriTabaniSifrelemeProje13;Integrated Security=True;");
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string AD = txtAd.Text;
+            byte[] ADdizi = ASCIIEncoding.UTF8.GetBytes(AD);
+
+            string sifreliAD =Convert.ToBase64String(ADdizi);
+
+            label6.Text = sifreliAD;
+
+
+            string soyad =txtSoyad.Text;
+            byte[] soyaddizi =ASCIIEncoding.UTF8.GetBytes(soyad);
+            string sifrelisoyad = Convert.ToBase64String(soyaddizi);
+
+
+            string mail = txtMail.Text;
+            byte[] maildizi = ASCIIEncoding.UTF8.GetBytes(mail);
+            string sifrelimail = Convert.ToBase64String(maildizi);
+
+
+            string sifre = txtSifre.Text;
+            byte[] sifredizi = ASCIIEncoding.UTF8.GetBytes(sifre);
+            string sifrelisifre = Convert.ToBase64String(sifredizi);
+
+
+            string hesapno = TxtHesapNo.Text;
+            byte[] hesapnodizi = ASCIIEncoding.UTF8.GetBytes(hesapno);
+            string sifrelihesapno = Convert.ToBase64String(hesapnodizi);
+
+
+            baglanti.Open();
+            SqlCommand komut = new SqlCommand("Insert into TBLVERILER(AD,SOYAD,MAIL,SIFRE,HESAPNO) values (@p1,@p2,@p3,@p4,@p5)",baglanti);
+
+            komut.Parameters.AddWithValue("@p1", sifreliAD);
+            komut.Parameters.AddWithValue("@p2", sifrelisoyad);
+            komut.Parameters.AddWithValue("@p3", sifrelimail);
+            komut.Parameters.AddWithValue("@p4", sifrelisifre);
+            komut.Parameters.AddWithValue("@p5", sifrelihesapno);
+
+            komut.ExecuteNonQuery();
+            baglanti.Close();
+
+            MessageBox.Show("VERİLER EKLENDİ");
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'veriTabaniSifrelemeProje13DataSet.TBLVERILER' table. You can move, or remove it, as needed.
+            this.tBLVERILERTableAdapter.Fill(this.veriTabaniSifrelemeProje13DataSet.TBLVERILER);
+
+        }
+    }
+}
