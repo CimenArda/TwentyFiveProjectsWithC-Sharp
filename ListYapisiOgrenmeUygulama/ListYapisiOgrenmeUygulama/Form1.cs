ba2476dfbb901446e6308d2e863d139b4ca4532d
ArdaCimen <cimenarda.yaz@gmail.com>
2024-07-21 13:10:15 +0300

    List Yapısı ve List ile Öğrenci Bilgileri Kaydetme Uygulaması

---
+++ ListYapisiOgrenmeUygulama/ListYapisiOgrenmeUygulama/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ListYapisiOgrenmeUygulama
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            List<string> karakterler = new List<string>();
+            karakterler.Add("Mazhar");
+            karakterler.Add("Ruhsar");
+            karakterler.Add("Menkıbe");
+            karakterler.Add("Ali");
+            karakterler.Add("Müfit");
+            karakterler.Add("Firdevs");
+
+            foreach (var item in karakterler)
+            {
+                listBox1.Items.Add(item);
+
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            List<int> sayilar = new List<int>();
+            sayilar.Add(1123);
+            sayilar.Add(345);
+            sayilar.Add(123);
+            sayilar.Add(645);
+            sayilar.Add(234);
+            sayilar.Add(6567);
+            sayilar.Add(1235634);
+
+            foreach (var item in sayilar)
+            {
+                if (item%5 ==0)
+                {
+                    listBox2.Items.Add(item);
+                }
+
+
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            List<Kisiler> kisi = new List<Kisiler>();
+
+            kisi.Add(new Kisiler()
+            {
+                ADI="Arda",
+                SOYADI="Cimen",
+                MESLEKI="Yazılımcı"
+            });
+            foreach (var item in kisi)
+            {
+                listBox3.Items.Add(item.ADI + " " +item.SOYADI + " :"+item.MESLEKI);
+            }
+        }
+    }
+}
---
+++ ListYapisiOgrenmeUygulama/ListYapisiOgrenmeUygulama/Kisiler.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListYapisiOgrenmeUygulama
+{
+    public class Kisiler
+    {
+        string ad, soyad, meslek;
+
+        public string ADI
+        {
+            get { return ad; }
+            set { ad = value; }
+        }
+        public string SOYADI
+        {
+            get { return soyad; }
+            set { soyad = value; }
+        }
+        public string MESLEKI
+        {
+            get { return meslek; }
+            set { meslek = value; }
+        }
+
+
+
+    }
+}
